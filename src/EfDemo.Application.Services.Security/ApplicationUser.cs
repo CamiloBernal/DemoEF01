2ec7b9e1703d598686ba80c3e1f3c2d1b6b45157
CamiloBernal <camilo.bernal@banlinea.com>
2016-06-30 09:49:04 -0500

    Remove IDentity Entity Framework nuget reference

--- src/EfDemo.Application.Services.Security/ApplicationUser.cs
+++ src/EfDemo.Application.Services.Security/ApplicationUser.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using EfDemo.Core.Model;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-
 
 namespace EfDemo.Application.Services.Security
 {
     public class ApplicationUser : User, IUser<long>
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser,long> manager)
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser, long> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
